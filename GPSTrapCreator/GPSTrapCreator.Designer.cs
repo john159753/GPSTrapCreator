38a56419b363bc68128760038efd8a26e76daa81
john159753 <jdaley1992@gmail.com>
2018-05-19 13:02:23 -0500

    Inital Commit
    Application useful for building CSV files for use with Raceboy

---
+++ GPSTrapCreator/GPSTrapCreator.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace GPSTrapCreator
+{
+    partial class GPSTrapCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapexplr = new GMap.NET.WindowsForms.GMapControl();
+            this.rdoZones = new System.Windows.Forms.RadioButton();
+            this.rdoAngles = new System.Windows.Forms.RadioButton();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.BtnPrepare = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.BtnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapexplr
+            // 
+            this.mapexplr.Bearing = 0F;
+            this.mapexplr.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.mapexplr.CanDragMap = true;
+            this.mapexplr.EmptyTileColor = System.Drawing.Color.Navy;
+            this.mapexplr.GrayScaleMode = false;
+            this.mapexplr.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.mapexplr.LevelsKeepInMemmory = 5;
+            this.mapexplr.Location = new System.Drawing.Point(12, 12);
+            this.mapexplr.MarkersEnabled = true;
+            this.mapexplr.MaxZoom = 1000;
+            this.mapexplr.MinZoom = 0;
+            this.mapexplr.MouseWheelZoomEnabled = true;
+            this.mapexplr.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.mapexplr.Name = "mapexplr";
+            this.mapexplr.NegativeMode = false;
+            this.mapexplr.PolygonsEnabled = true;
+            this.mapexplr.RetryLoadTile = 0;
+            this.mapexplr.RoutesEnabled = true;
+            this.mapexplr.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.mapexplr.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.mapexplr.ShowTileGridLines = false;
+            this.mapexplr.Size = new System.Drawing.Size(1313, 1528);
+            this.mapexplr.TabIndex = 0;
+            this.mapexplr.Zoom = 0D;
+            this.mapexplr.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.mapMarkerClicked);
+            this.mapexplr.Load += new System.EventHandler(this.mapexplr_Load_1);
+            this.mapexplr.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mapexplr_MouseDoubleClick);
+            // 
+            // rdoZones
+            // 
+            this.rdoZones.AutoSize = true;
+            this.rdoZones.Checked = true;
+            this.rdoZones.Location = new System.Drawing.Point(1547, 24);
+            this.rdoZones.Name = "rdoZones";
+            this.rdoZones.Size = new System.Drawing.Size(144, 29);
+            this.rdoZones.TabIndex = 2;
+            this.rdoZones.TabStop = true;
+            this.rdoZones.Text = "Lay Points";
+            this.rdoZones.UseVisualStyleBackColor = true;
+            this.rdoZones.CheckedChanged += new System.EventHandler(this.rdoZones_CheckedChanged);
+            // 
+            // rdoAngles
+            // 
+            this.rdoAngles.AutoSize = true;
+            this.rdoAngles.Location = new System.Drawing.Point(1547, 59);
+            this.rdoAngles.Name = "rdoAngles";
+            this.rdoAngles.Size = new System.Drawing.Size(360, 29);
+            this.rdoAngles.TabIndex = 3;
+            this.rdoAngles.Text = "Lay Angles (Lines up with points)";
+            this.rdoAngles.UseVisualStyleBackColor = true;
+            this.rdoAngles.CheckedChanged += new System.EventHandler(this.rdoAngles_CheckedChanged);
+            // 
+            // dg1
+            // 
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Location = new System.Drawing.Point(1331, 126);
+            this.dg1.Name = "dg1";
+            this.dg1.RowTemplate.Height = 33;
+            this.dg1.Size = new System.Drawing.Size(1167, 625);
+            this.dg1.TabIndex = 4;
+            // 
+            // BtnPrepare
+            // 
+            this.BtnPrepare.Location = new System.Drawing.Point(2322, 757);
+            this.BtnPrepare.Name = "BtnPrepare";
+            this.BtnPrepare.Size = new System.Drawing.Size(176, 48);
+            this.BtnPrepare.TabIndex = 5;
+            this.BtnPrepare.Text = "Prepare";
+            this.BtnPrepare.UseVisualStyleBackColor = true;
+            this.BtnPrepare.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1331, 757);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(176, 48);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(1331, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(187, 31);
+            this.txtSearch.TabIndex = 10;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(1331, 72);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(176, 48);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // BtnExport
+            // 
+            this.BtnExport.Location = new System.Drawing.Point(2322, 811);
+            this.BtnExport.Name = "BtnExport";
+            this.BtnExport.Size = new System.Drawing.Size(176, 48);
+            this.BtnExport.TabIndex = 11;
+            this.BtnExport.Text = "Export CSV";
+            this.BtnExport.UseVisualStyleBackColor = true;
+            this.BtnExport.Click += new System.EventHandler(this.BtnToCsv_Click);
+            // 
+            // GPSTrapCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(2613, 1783);
+            this.Controls.Add(this.BtnExport);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.BtnPrepare);
+            this.Controls.Add(this.dg1);
+            this.Controls.Add(this.rdoAngles);
+            this.Controls.Add(this.rdoZones);
+            this.Controls.Add(this.mapexplr);
+            this.Name = "GPSTrapCreator";
+            this.Text = "GPS Trap Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl mapexplr;
+        private System.Windows.Forms.RadioButton rdoZones;
+        private System.Windows.Forms.RadioButton rdoAngles;
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.Button BtnPrepare;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button BtnExport;
+    }
+}
+
---
+++ GPSTrapCreator/GPSTrapCreator.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using System.IO;
+
+
+namespace GPSTrapCreator
+{
+    public partial class GPSTrapCreator : Form
+    {
+        GMapOverlay overlayOne;
+        GMapOverlay overlayTwo;
+        GMapOverlay polyOverlay;
+        List<gpsZone> Zones = new List<gpsZone>();
+        double horizontalSpread = 0.00023;
+        double verticalSpread = 0.00006;
+        List<CsvOutput> csvZones = new List<CsvOutput>();
+        public GPSTrapCreator()
+        {
+            InitializeComponent();
+        }
+
+
+        private void mapexplr_Load_1(object sender, EventArgs e)
+        {
+            mapexplr.SetPositionByKeywords("Gingerman Raceway");
+
+            mapexplr.MapProvider = GMap.NET.MapProviders.BingSatelliteMapProvider.Instance;
+            mapexplr.MinZoom = 3;
+            mapexplr.MaxZoom = 20;
+            mapexplr.Zoom = 16;
+            mapexplr.Manager.Mode = AccessMode.ServerAndCache;
+
+            overlayOne = new GMapOverlay("Trap Locations");
+            overlayTwo = new GMapOverlay("Trap Locations");
+            polyOverlay = new GMapOverlay("polygons");
+  
+        }
+
+        private void mapexplr_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (rdoZones.Checked)
+            {
+
+                var mCoord = mapexplr.FromLocalToLatLng(e.X, e.Y);
+                var gmg = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new PointLatLng(mCoord.Lat, mCoord.Lng), GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green);
+                mapexplr.Overlays.Add(overlayOne);
+                overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new PointLatLng(mCoord.Lat, mCoord.Lng), GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green));
+                dg1.DataSource = null;
+                dg1.DataSource = overlayOne.Markers;
+                
+
+            }
+            else
+            {
+                var mCoord = mapexplr.FromLocalToLatLng(e.X, e.Y);
+                var gmg = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new PointLatLng(mCoord.Lat, mCoord.Lng), GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green);
+                mapexplr.Overlays.Add(overlayTwo);
+                overlayTwo.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new PointLatLng(mCoord.Lat, mCoord.Lng), GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_dot));
+                dg1.DataSource = null;
+                dg1.DataSource = overlayTwo.Markers;
+
+            }
+            
+        }
+   
+        private void mapMarkerClicked(GMapMarker item, MouseEventArgs e)
+        {
+            var msgBox = MessageBox.Show("Are you sure you would like to remove this marker?","Confirmation", MessageBoxButtons.YesNo);
+            if (msgBox == DialogResult.Yes)
+            {
+                if (rdoZones.Checked)
+                {
+
+                    overlayOne.Markers.Remove(item);
+                    dg1.DataSource = null;
+                    dg1.DataSource = overlayOne.Markers;
+
+                }
+                else
+                {
+                    overlayTwo.Markers.Remove(item);
+                    dg1.DataSource = null;
+                    dg1.DataSource = overlayTwo.Markers;
+                }
+            }  
+        }
+
+
+
+        private void rdoAngles_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoZones.Checked)
+            {
+                dg1.DataSource = null;
+                dg1.DataSource = overlayOne.Markers;
+
+
+            }
+            else
+            {
+                dg1.DataSource = null;
+                dg1.DataSource = overlayTwo.Markers;
+            }
+        }
+
+        private void rdoZones_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoZones.Checked)
+            {
+                dg1.DataSource = null;
+                dg1.DataSource = overlayOne.Markers;
+                
+            }
+            else
+            {
+                dg1.DataSource = null;
+                dg1.DataSource = overlayTwo.Markers;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            List<gpsZone> preZones = new List<gpsZone>();
+            List<List<PointLatLng>> mapPolys = new List<List<PointLatLng>>();
+            for (int i = 0; i < overlayOne.Markers.Count(); i++)
+            {
+                var pnt = overlayOne.Markers[i];
+                var ang = overlayTwo.Markers[i];
+
+                var at2 = Math.Atan2((ang.Position.Lng - pnt.Position.Lng), (ang.Position.Lat - pnt.Position.Lat));
+
+                if (at2 < 0)
+                    at2 += (Math.PI) * 2;
+
+                gpsZone zone = new gpsZone();
+                zone.Lat = pnt.Position.Lat;
+                zone.Lng = pnt.Position.Lng;
+                zone.headingAngle = (int)(at2 * 57.2957795130823209);
+                preZones.Add(zone);
+            }
+            foreach (var zone in preZones)
+            {
+                List<PointLatLng> points = new List<PointLatLng>();
+                points.Add(new PointLatLng(zone.Lat - (verticalSpread / 2), zone.Lng - (horizontalSpread / 2)));
+                points.Add(new PointLatLng(zone.Lat + (verticalSpread / 2), zone.Lng - (horizontalSpread / 2)));
+                points.Add(new PointLatLng(zone.Lat + (verticalSpread / 2), zone.Lng + (horizontalSpread / 2)));
+                points.Add(new PointLatLng(zone.Lat - (verticalSpread / 2), zone.Lng + (horizontalSpread / 2)));
+                
+
+                if (zone.headingAngle != 0)
+                {
+                    List<PointLatLng> tmppoints = new List<PointLatLng>();
+                    foreach (var pnt in points)
+                    {
+                        var tranformed = TransformPoint(pnt.Lat, pnt.Lng, zone.headingAngle, zone.Lat, zone.Lng);
+                        tmppoints.Add(new PointLatLng(tranformed["X"], tranformed["Y"]));
+                    }
+                    points.Clear();
+                    points = tmppoints;
+                }
+                mapPolys.Add(points);
+            }
+            int zoneCount = 0;
+            foreach(var poly in mapPolys)
+            {
+                GMapPolygon zoneBounds = new GMapPolygon(poly, $"Zone {zoneCount}");
+                zoneBounds.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+                zoneBounds.Stroke = new Pen(Color.Red, 1);
+                polyOverlay.Polygons.Add(zoneBounds);
+                mapexplr.Overlays.Add(polyOverlay);
+                
+                zoneCount++;
+            }
+
+            
+            foreach (var p in polyOverlay.Polygons)
+            {
+                CsvOutput zone = new CsvOutput();
+
+
+                zone.ZoneName = p.Name;
+
+                var lats = p.Points.Select(lat => (int)(lat.Lat * 10000000));
+                var lngs = p.Points.Select(lng => (int)(lng.Lng * 10000000));
+
+                zone.Lat1 = lats.ToArray()[0];
+                zone.Lat2 = lats.ToArray()[1];
+                zone.Lat3 = lats.ToArray()[2];
+                zone.Lat4 = lats.ToArray()[3];
+                zone.Lng1 = lngs.ToArray()[0];
+                zone.Lng2 = lngs.ToArray()[1];
+                zone.Lng3 = lngs.ToArray()[2];
+                zone.Lng4 = lngs.ToArray()[3];
+
+
+
+                zone.ZoneCount = polyOverlay.Polygons.Count;
+                csvZones.Add(zone);
+            }
+
+
+            rdoAngles.Enabled = false;
+            rdoZones.Enabled = false;
+            dg1.DataSource = null;
+            dg1.DataSource = csvZones;
+            MessageBox.Show("Processing Done, You can export now.");
+        }
+
+        private Dictionary<string,double> TransformPoint(double x1, double y1, int rotation, double midX, double midY )
+        {
+            Dictionary<string, double> result = new Dictionary<string, double>();
+
+            double dx = x1 - midX;
+            double dy = y1 - midY;
+            double radians = DegreeToRadian(rotation);
+            x1 = (dx * Math.Cos(radians)) - (dy * Math.Sin(radians));
+            y1 = (dx * Math.Sin(radians)) + (dy * Math.Cos(radians));
+
+            x1 = x1 + midX;
+            y1 = y1 + midY;
+
+            result.Add("X", x1);
+            result.Add("Y", y1);
+            return result;
+    }
+        private double DegreeToRadian(int angle)
+        {
+
+            return (Math.PI * angle / 180.0);
+        }
+        public class gpsZone
+        {
+
+            public double Lat { get; set; }
+            public double Lng { get; set; }
+            public int headingAngle { get; set; }
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            overlayOne.Clear();
+            overlayTwo.Clear();
+            polyOverlay.Clear();
+            Zones.Clear();
+            csvZones.Clear();
+            rdoAngles.Enabled = true;
+            rdoZones.Enabled = true;
+            rdoZones.Select();
+            dg1.DataSource = null;
+            dg1.DataSource = overlayOne.Markers;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var searchResult = mapexplr.SetPositionByKeywords(txtSearch.Text);
+            if (searchResult == GeoCoderStatusCode.G_GEO_SUCCESS)
+            {
+                mapexplr.Zoom = 16;
+            }
+            else
+            {
+                MessageBox.Show(searchResult.ToString());
+            }
+            
+        }
+
+
+         void tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSearch_Click(null, null);
+            }
+        }
+
+        private void BtnToCsv_Click(object sender, EventArgs e)
+        {
+            
+         
+            
+
+       
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "CSV (*.csv)|*.csv|All files (*.*)|*.*";
+            save.InitialDirectory = @"C:\Users\John\Documents\";
+            save.Title = "Save Trap File";
+            save.ShowDialog();
+
+
+
+            // If the file name is not an empty string open it for saving.  
+            if (save.FileName != "")
+            {
+                using (StreamWriter outputFile = new StreamWriter(save.FileName))
+                {
+                    foreach(var line in csvZones )
+                    {
+                        outputFile.WriteLine($"{line.ZoneName},{line.Lat1},{line.Lat2},{line.Lat3},{line.Lat4},{line.Lng1},{line.Lng2},{line.Lng3},{line.Lng4},{line.ZoneCount}");
+                    }
+                    
+                }
+            }
+
+        }
+    }
+    public class CsvOutput
+    {
+
+        // ZoneName,Lat1,Lat2,Lat3,Lat4,Lng1,Lng2,Lng3,Lng4,ZoneCount
+        public string ZoneName { get; set; }
+        public int Lat1 { get; set; }
+        public int Lat2 { get; set; }
+        public int Lat3 { get; set; }
+        public int Lat4 { get; set; }
+        public int Lng1 { get; set; }
+        public int Lng2 { get; set; }
+        public int Lng3 { get; set; }
+        public int Lng4 { get; set; }
+        public int ZoneCount { get; set; }
+       
+
+    }
+
+    }
